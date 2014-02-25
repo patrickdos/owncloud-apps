c3162718e7cb5011976e7f867545b2f374a848f7
Miroslav Bauer <bauer@cesnet.cz>
2014-02-25 14:00:18 +0100

    USER_SAML: Sending email when user autocreated, password changed

--- user_saml/appinfo/app.php
+++ user_saml/appinfo/app.php
@@ -43,6 +43,7 @@ if (OCP\App::isEnabled('user_saml')) {
 
 	OC::$CLASSPATH['OC_USER_SAML_Hooks'] = 'user_saml/lib/hooks.php';
 	OCP\Util::connectHook('OC_User', 'post_createUser', 'OC_USER_SAML_Hooks', 'post_createUser');
+	OCP\Util::connectHook('OC_User', 'post_setPassword', 'OC_USER_SAML_Hooks', 'post_setPassword');
 	OCP\Util::connectHook('OC_User', 'post_login', 'OC_USER_SAML_Hooks', 'post_login');
 	OCP\Util::connectHook('OC_User', 'logout', 'OC_USER_SAML_Hooks', 'logout');
 
