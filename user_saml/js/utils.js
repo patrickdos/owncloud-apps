d81f5f074df042fb8a68c5b57bccfccab0fae94e
Miroslav Bauer <bauer@cesnet.cz>
2014-02-25 14:55:58 +0100

    USER_SAML: Custom login page and translations

--- user_saml/js/utils.js
+++ user_saml/js/utils.js
@@ -1,66 +1,41 @@
 (function() {
-    
 	var saml = document.createElement('script');
 	saml.type = 'text/javascript';
 	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(saml);
 })();
 
 $(document).ready(function(){
+	$('<div id="login-saml"></div>').css({'text-align': 'center',}).appendTo('form');
 
-	var loginMsg = t('user_saml', 'Login with SAML');
+	var loginMsg = t("user_saml","Log in with your eduID account");
+	$('#user').parent().remove();
+	$('#password').parent().remove();
+	$('#remember_login').parent().remove();
+	$('#remember_login+label').parent().remove();
+	$('#submit').remove();
 
-    $('<div id="login-saml"></div>').css({
-		'text-align': 'center',
-    }).appendTo('form');
-
-	$('<p>or</p>').css(
+	$('<a id="login-saml-action" href="simplesamlphp/module.php/core/as_login.php?AuthId=default-sp&ReturnTo=https://'+escapeHTML(document.domain)+'?app=user_saml" ></a>').css(
 	{
-		'text-align': 'center',
-        'font-weight': 'bolder',
-        'font-size' : '110%'
+		'text-decoration': 'none'
 	}).appendTo('#login-saml');
 
-    if ($('#user').val() == "") {
-        $('#password').parent().hide();
-        $('#remember_login').hide();
-        $('#remember_login+label').hide();
-        $('#submit').hide();
-    }
-
-    $('#user').change( function() {
-        if ($(this).val() !== "") {
-            $('#password').parent().show();
-            $('#remember_login').show();
-            $('#remember_login+label').show();
-            $('#submit').show();
-        }
-        else {
-            $('#password').parent().hide();
-            $('#remember_login').hide();
-            $('#remember_login+label').hide();
-            $('#submit').hide();
-        }
-    });
-
-	$('<p>Access using SAML authentication</p>').css(
+	$('<p></p>').css(
 	{
 		'text-align': 'center',
-        'font-weight': 'bolder',
-        'font-size' : '110%'
-	}).appendTo('#login-saml');
+		'font-weight': 'bolder',
+		'font-size' : '110%',
+		'color' : '#fafaff'
+	}).appendTo('#login-saml').text(loginMsg);
 
-
-    $('<a id="login-saml-action" href="simplesamlphp/module.php/core/as_login.php?AuthId=default-sp&ReturnTo=https://'+escapeHTML(document.domain)+'?app=user_saml" ></a>').css(
-    {
-        'text-decoration': 'none'
-    }).appendTo('#login-saml');
-
-
-	$('<img id="login-saml-img" src="' + OC.imagePath('user_saml', 'logo.jpg') + '" title="'+ loginMsg +'" alt="'+ loginMsg +'" />').css(
+	$('<img id="login-saml-img" src="' + OC.imagePath('user_saml', 'eduid.png') + '" title="'+ loginMsg +'" alt="'+ loginMsg +'" />').css(
 	{
-		cursor : 'pointer',
-        border : '1px solid #777'
+		'cursor' : 'pointer',
+		'border' : '3px solid #777',
+		'border-radius' : '15px',
+		'-moz-border-radius' : '15px'
 	}).appendTo('#login-saml-action');
 
-
+	var termsLink = '<a href="https://du.cesnet.cz/wiki/doku.php/cs/provozni_pravidla">'+escapeHTML(t('user_saml','terms of use'))+'</a>'
+	var legalNotice = t("user_saml", "By logging in you agree with")+termsLink;
+	$('<p class="info"></p>').appendTo('#login-saml').html(legalNotice);
 });
