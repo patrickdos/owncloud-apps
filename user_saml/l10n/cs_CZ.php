1c638afa2204667f3494bcba2a9d83983968d373
Miroslav Bauer <bauer@cesnet.cz>
2014-02-25 13:10:23 +0100

    USER_SAML: added cs_CZ localization

---
+++ user_saml/l10n/cs_CZ.php
@@ -0,0 +1,17 @@
+<?php $TRANSLATIONS = array(
+"Access credentials for sync apps" => "Přihlašovací údaje pro synchronizační aplikaci",
+"Set password for your sync app here" => "Zde si nastavte přístupové heslo pro aplikaci",
+"Username" => "Uživatelské jméno",
+"Password" => "Heslo",
+"Set password" => "Nastavit heslo",
+"Repeat password" => "Zopakovat heslo",
+"Password successfully set!" => "Heslo bylo úspěšně nastaveno",
+"Passwords doesn't match" => "Hesla se neshodují",
+"Unable to set password" => "Nastavení hesla se nezdařilo. Zkuste to prosím znovu",
+"Log in with your eduID account" => "Přihlaste se účtem v eduID",
+"%s account creation" => "Založení %s účtu",
+"%s password change" => "Vaše %s heslo bylo změněno",
+"By logging in you agree with" => "Přihlášením souhlasíte s",
+"terms of use" => "pravidly použití",
+"E-mail notifications will be sent to this address" => "Na tuto adresu budou zasílány e-mailové notifikace",
+);
